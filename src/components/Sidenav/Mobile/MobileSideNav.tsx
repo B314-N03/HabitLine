7487455760ca9caaebab4eef4c1d06bd727dff45
B314-N03-Ubuntu <bela-noe@web.de>
2025-07-29 23:19:41 +0200

    minor bug fix

--- src/components/Sidenav/Mobile/MobileSideNav.tsx
+++ src/components/Sidenav/Mobile/MobileSideNav.tsx
@@ -39,7 +39,7 @@ function MobileSideNav() {
                     },
                 }}
             >
-                <DrawerWebapp hideText />
+                <DrawerWebapp />
             </Drawer>
         </Box>
     )
