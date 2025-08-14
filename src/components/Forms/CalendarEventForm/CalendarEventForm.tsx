ffc864b80fe4cd11f80b249d1ea3954e0e8eace3
B314-Win10 <bela-noe@web.de>
2025-08-15 00:59:58 +0200

    minor: add event Duration suggestions to the modal

--- src/components/Forms/CalendarEventForm/CalendarEventForm.tsx
+++ src/components/Forms/CalendarEventForm/CalendarEventForm.tsx
@@ -5,6 +5,8 @@ import CalendarEventDateTimeInput from './CalendarEventDateTimeInput/CalendarEve
 import CalendarEventTitleInput from './CalendarEventTitleInput/CalendarEventTitleInput';
 import CalendarEventCalendarInput from './CalendarEventCalendarInput/CalendarEventCalendarInput';
 import CalendarEventPersonsInput from './CalendarEventPersonsInput/CalendarEventPersonsInput';
+import IconButtonHL from '../../Widgets/Buttons/IconButton';
+import { LockClock } from '@mui/icons-material';
 
 interface CalendarEventFormProps {
     title: string;
@@ -25,12 +27,53 @@ interface CalendarEventFormProps {
 }
 
 function CalendarEventForm({ title, setTitle, startDate, setStartDate, endDate, setEndDate, calendarId, setCalendarId, people, setPeople, newPerson, setNewPerson, calendars, showAddNewCalendar, setShowAddNewCalendar }: CalendarEventFormProps) {
+
+    const eventDurationSuggestion = [
+        {
+            label: "15 min",
+            value: 15
+        },
+        {
+            label: "30 min",
+            value: 30
+        },
+        {
+            label: "1 hour",
+            value: 60
+        },
+        {
+            label: "2 hours",
+            value: 120
+        },
+    ]
+
+    const handleEventDurationClick = (value: number) => {
+        const start = new Date(startDate);
+        start.setMinutes(start.getMinutes() + value);
+        const pad = (n: number) => n.toString().padStart(2, '0');
+        const endTimeStr = `${start.getFullYear()}-${pad(start.getMonth() + 1)}-${pad(start.getDate())} ${pad(start.getHours())}:${pad(start.getMinutes())}`;
+        setEndDate(endTimeStr);
+    }
+
     return (
         <div className={styles.calendarEventForm}>
             <CalendarEventTitleInput title={title} setTitle={setTitle} />
-            <div style={{ display: "flex", gap: 16 }}>
-                <CalendarEventDateTimeInput label="Start" date={startDate} setDate={setStartDate} />
-                <CalendarEventDateTimeInput label="End" date={endDate} setDate={setEndDate} />
+            <div style={{ display: "flex", flexDirection: "column" }}>
+                <div style={{ display: "flex", gap: 16 }}>
+                    <CalendarEventDateTimeInput label="Start" date={startDate} setDate={setStartDate} />
+                    <CalendarEventDateTimeInput label="End" date={endDate} setDate={setEndDate} />
+                </div>
+                <div style={{ display: "flex", flexWrap: "wrap", gap: 8, marginTop: 8 }}>
+                    {eventDurationSuggestion.map((suggestion) => (
+                        <IconButtonHL
+                            variant='outlined'
+                            icon={<LockClock />}
+                            key={suggestion.label}
+                            customClass={styles.eventDurationSuggestion}
+                            title={suggestion.label}
+                            onClick={() => handleEventDurationClick(suggestion.value)} />
+                    ))}
+                </div>
             </div>
             <div style={{ display: "flex", gap: 16 }}>
                 <CalendarEventCalendarInput
